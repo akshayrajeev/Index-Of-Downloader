507296ea974402c8168e824ce9e15be1b0595c92
akshayrajeev <akshayrajeev221b@gmail.com>
2019-03-03 14:47:44 +0530

    Added Progress Bar And Switched To Requests Module

--- IndexDownloader.py
+++ IndexDownloader.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
-import wget
+from clint.textui import progress
 
 links = []
+chunk_size = 256
+
 print("Enter 'Index Of' URL")
 url = input()
 print("Enter The Path To Save Files")
@@ -13,8 +15,19 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 for link in (soup.find_all('a')):
     links.append(link['href'])
+    
+print("")
 
 for link in links:
-    download_url = url + "/" + link
-    wget.download(download_url,path)
+    req = requests.get(url + '/' + link, stream = True)
+    try:
+        with open(path + link, "wb") as file:
+            length = int(req.headers.get('content-length'))
+            for chunk in progress.bar(req.iter_content(chunk_size), expected_size = (length/chunk_size), label = link + "  "):
+                if chunk:
+                    file.write(chunk)
+            print("Download Successful\n")
+            
+    except OSError as e:
+        continue
 
